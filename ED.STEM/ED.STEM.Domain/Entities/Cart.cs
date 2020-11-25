a2728f2d9cea4372be0eb956977e428e2c4f0a5c
DESKTOP-D85I4D5\ZhiHua <yunhual@yahoo.com>
2020-11-24 23:34:19 -0500

    1.[NEW] AddToCart, display cart content

---
+++ ED.STEM/ED.STEM.Domain/Entities/Cart.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ED.STEM.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+        public void AddItem(STEMProgram product, int quantity)
+        {
+            CartLine line = lineCollection
+            .Where(p => p.Product.STEMProgramId == product.STEMProgramId)
+            .FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+        public void RemoveLine(STEMProgram product)
+        {
+            lineCollection.RemoveAll(l => l.Product.STEMProgramId == product.STEMProgramId);
+        }
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(e => e.Product.Price * e.Quantity);
+        }
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+    public class CartLine
+    {
+        public STEMProgram Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ ED.STEM/ED.STEM.WebApp/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using ED.STEM.Domain.Abstract;
+using ED.STEM.Domain.Concrete;
+using ED.STEM.Domain.Entities;
+using ED.STEM.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ED.STEM.WebApp.Controllers
+{
+    public class CartController : Controller
+    {
+        public ISTEMProgramsRepository ProgramsRepository { get; set; }
+          = new EFSTEMProgramRepository();
+        public RedirectToRouteResult AddToCart(int STEMProgramId, string returnUrl)
+        {
+            STEMProgram product = ProgramsRepository
+            .STEMPrograms
+            .FirstOrDefault(p => p.STEMProgramId == STEMProgramId);
+            if (product != null)
+            {
+                GetCart().AddItem(product, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        public RedirectToRouteResult RemoveFromCart(int STEMProgramId, string returnUrl)
+        {
+            STEMProgram product = ProgramsRepository
+           .STEMPrograms
+           .FirstOrDefault(p => p.STEMProgramId == STEMProgramId);
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+    }
+}
